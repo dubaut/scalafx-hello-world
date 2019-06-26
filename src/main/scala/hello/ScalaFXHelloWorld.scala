1986353136e3adea3b1e19d8c0f500f31dd26990
Hannes <hannes@buildingdragons.dev>
2019-06-26 21:34:07 +0200

    Modified to a sample application not using ScalaFX.

--- src/main/scala/hello/ScalaFXHelloWorld.scala
+++ src/main/scala/hello/ScalaFXHelloWorld.scala
@@ -27,49 +27,23 @@
 
 package hello
 
-import scalafx.application.JFXApp
-import scalafx.application.JFXApp.PrimaryStage
-import scalafx.geometry.Insets
-import scalafx.scene.Scene
-import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
-import scalafx.scene.paint.Color._
-import scalafx.scene.paint._
-import scalafx.scene.text.Text
+import javafx.application.{Application, Platform}
+import javafx.scene.Scene
+import javafx.scene.control.Label
+import javafx.stage.Stage
 
-object ScalaFXHelloWorld extends JFXApp {
+class ScalaFXHelloWorld extends Application {
+  override def start(stage: Stage): Unit = {
+    stage.setTitle("Does it work?")
+    stage.setScene(new Scene(new Label("It works!")))
+    stage.show()
 
-  stage = new PrimaryStage {
-    //    initStyle(StageStyle.Unified)
-    title = "ScalaFX Hello World"
-    scene = new Scene {
-      fill = Color.rgb(38, 38, 38)
-      content = new HBox {
-        padding = Insets(50, 80, 50, 80)
-        children = Seq(
-          new Text {
-            text = "Scala"
-            style = "-fx-font: normal bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(Red, DarkRed))
-          },
-          new Text {
-            text = "FX"
-            style = "-fx-font: italic bold 100pt sans-serif"
-            fill = new LinearGradient(
-              endX = 0,
-              stops = Stops(White, DarkGray)
-            )
-            effect = new DropShadow {
-              color = DarkGray
-              radius = 15
-              spread = 0.25
-            }
-          }
-        )
-      }
-    }
+    Platform.exit()
+  }
+}
 
+object ScalaFXHelloWorld {
+  def main(args: Array[String]): Unit = {
+    Application.launch(classOf[ScalaFXHelloWorld], args: _*)
   }
 }
